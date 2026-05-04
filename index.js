440a7f6b45c85b3bd6a7a9ac032d111e23c89d58
alexrivas05 <alex12rivas34@gmail.com>
2026-05-04 10:31:25 -0400

    Initial commit. Initial html and js files created.

---
+++ index.js
@@ -0,0 +1,8 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+app.on('ready', ()=>{
+    console.log('app is ready');
+    const mainWindow = newWindow();
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+});
